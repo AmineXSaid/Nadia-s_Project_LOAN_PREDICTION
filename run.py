122a1ca6fe6a6fac22e1e0f5328626d49c62cfc6
RoyMustang-bit <55053638+RoyMustang-bit@users.noreply.github.com>
2021-08-30 20:27:27 +0100

    run_nadia_run.py

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 import pickle
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 import base64
 
 
@@ -118,8 +114,6 @@ elif app_mode =='Prediction':
     f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
     unsafe_allow_html=True,
     )
-else : 
-    st.write('2')
 
 
 
