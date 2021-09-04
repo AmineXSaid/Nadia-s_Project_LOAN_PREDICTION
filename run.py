35bf45d2d75be4c41909a4924c92b2a2216cca32
RoyMustang-bit <55053638+RoyMustang-bit@users.noreply.github.com>
2021-09-04 10:21:44 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,31 +1,37 @@
 import streamlit as st
+import pandas as pd
 import numpy as np
 import pickle
 import base64
-from sklearn.ensemble import RandomForestClassifier
+
 
 
 @st.cache(suppress_st_warning=True)
 def get_fvalue(val):
-	feature_dict = {"No":1,"Yes":2}
-	for key,value in feature_dict.items():
-		if val == key:
-			return value
+    feature_dict = {"No":1,"Yes":2}
+    for key,value in feature_dict.items():
+        if val == key:
+            return value
 
 def get_value(val,my_dict):
-	for key,value in my_dict.items():
-		if val == key:
-			return value 
-        
+    for key,value in my_dict.items():
+        if val == key:
+            return value
+       
 app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction'])
 if app_mode=='Home':
     st.title('LOAN PREDICTION :')
-    st.image('hipster_loan-1.jpg')   
-    
-    
-    
+    st.image('hipster_loan-1.jpg') 
+    st.write('App realised by : Nadia Mhadhbi') 
+   
+   
+   
 elif app_mode =='Prediction':
-    st.subheader('Sir/Mme , YOU need to fill all neccesary informations in order to get a reply to your loan request :')
+    
+    
+    st.image('slider-short-3.jpg')
+
+    st.subheader('Sir/Mme , YOU need to fill all neccesary informations in order to get a reply to your loan request !')
     st.sidebar.header("Informations about the client :")
     gender_dict = {"Male":1,"Female":2}
     feature_dict = {"No":1,"Yes":2}
@@ -34,13 +40,13 @@ elif app_mode =='Prediction':
     Gender=st.sidebar.radio('Gender',tuple(gender_dict.keys()))
     Married=st.sidebar.radio('Married',tuple(feature_dict.keys()))
     Self_Employed=st.sidebar.radio('Self Employed',tuple(feature_dict.keys()))
-    Dependents=st.sidebar.selectbox('Dependents',options=['0','1' , '2' , '3+'])
-    Education=st.sidebar.selectbox('Education',tuple(edu.keys()))
+    Dependents=st.sidebar.radio('Dependents',options=['0','1' , '2' , '3+'])
+    Education=st.sidebar.radio('Education',tuple(edu.keys()))
     ApplicantIncome=st.sidebar.slider('ApplicantIncome',0,10000,0,)
     CoapplicantIncome=st.sidebar.slider('CoapplicantIncome',0,10000,0,)
     LoanAmount=st.sidebar.slider('LoanAmount in K$',9.0,700.0,200.0)
-    Loan_Amount_Term=st.sidebar.selectbox('Loan_Amount_Term',(12.0,36.0,60.0,84.0,120.0,180.0,240.0,300.0,360.0)) 
-    Credit_History=st.sidebar.selectbox('Credit_History',(0.0,1.0))
+    Loan_Amount_Term=st.sidebar.selectbox('Loan_Amount_Term',(12.0,36.0,60.0,84.0,120.0,180.0,240.0,300.0,360.0))
+    Credit_History=st.sidebar.radio('Credit_History',(0.0,1.0))
     Property_Area=st.sidebar.radio('Property_Area',tuple(prop.keys()))
 
 
@@ -61,11 +67,11 @@ elif app_mode =='Prediction':
         Semiurban = 1
     else :
         Rural=1
-    
+   
     data1={
     'Gender':Gender,
     'Married':Married,
-    'Dependents':[class_0,class_1,class_2,class_3], 
+    'Dependents':[class_0,class_1,class_2,class_3],
     'Education':Education,
     'ApplicantIncome':ApplicantIncome,
     'CoapplicantIncome':CoapplicantIncome,
@@ -85,14 +91,14 @@ elif app_mode =='Prediction':
         contents = file_.read()
         data_url = base64.b64encode(contents).decode("utf-8")
         file_.close()
-    
+   
         file = open("green-cola-no.gif", "rb")
         contents = file.read()
         data_url_no = base64.b64encode(contents).decode("utf-8")
         file.close()
-    
-    
-        loaded_model = pickle.load(open('Random_Forest.sav', 'rb')) 
+   
+   
+        loaded_model = pickle.load(open('Random_Forest.sav', 'rb'))
         prediction = loaded_model.predict(single_sample)
         if prediction[0] == 0 :
             st.error(
@@ -109,9 +115,6 @@ elif app_mode =='Prediction':
     f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
     unsafe_allow_html=True,
     )
-
-
-
 
 
 
